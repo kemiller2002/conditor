09bf34c37bd035ae22ab521512b24f84debb29d4
Kevin Miller <kemiller2002@gmail.com>
2026-09-24 09:44:27 +0200

    Separate capability version from distribution source

--- src/Conditor.Core/Registry.fs
+++ src/Conditor.Core/Registry.fs
@@ -1,11 +1,12 @@
 namespace Conditor.Core
 
 module Registry =
-    let private lifecycle id displayName package command version initArgs =
+    let private lifecycle id displayName package source command version initArgs =
         { Id = id
           DisplayName = displayName
           Distribution = LifecycleNpm
           Package = package
+          LifecycleSource = Some source
           Command = Some command
           DefaultVersion = version
           InitArguments = initArgs
@@ -17,6 +18,7 @@ module Registry =
           DisplayName = displayName
           Distribution = NpmPackage
           Package = package
+          LifecycleSource = None
           Command = None
           DefaultVersion = version
           InitArguments = []
@@ -28,6 +30,7 @@ module Registry =
           DisplayName = displayName
           Distribution = NugetPackage
           Package = package
+          LifecycleSource = None
           Command = None
           DefaultVersion = version
           InitArguments = []
@@ -39,14 +42,23 @@ module Registry =
               "praxis"
               "Praxis / Repository Operating System"
               "@echelon-foundry/repository-operating-system"
+              RegistryPackage
               "ros"
-              "3.4.0"
+              "3.1.4"
               [ "init"; "--target"; "{target}" ]
-          lifecycle "ordo" "Ordo / State-Directed Engineering" "@echelon-foundry/sde" "sde" "1.4.0" [ "init" ]
+          lifecycle
+              "ordo"
+              "Ordo / State-Directed Engineering"
+              "@echelon-foundry/sde"
+              RegistryPackage
+              "sde"
+              "1.3.0"
+              [ "init" ]
           lifecycle
               "visual-engineering"
               "Visual Engineering"
               "@echelon-foundry/visual-engineering"
+              RegistryPackage
               "visual-engineering"
               "1.0.0"
               [ "init" ]
@@ -54,6 +66,7 @@ module Registry =
               "communication-engineering"
               "Communication Engineering"
               "@echelon-foundry/communication-engineering"
+              (FixedPackageSpec "github:kemiller2002/communication-engineering#4590d2fe6f7e80b339117d3fbee5803f2dd39122")
               "communication-engineering"
               "1.0.0"
               [ "init" ]
@@ -61,8 +74,9 @@ module Registry =
               "limen"
               "Limen"
               "@echelon-foundry/typescript-wasm-kernel"
+              RegistryPackage
               "limen"
-              "0.6.2"
+              "0.6.1"
               [ "init" ]
           npmPackage "forma" "Forma" "@echelon-foundry/design-system" "0.2.0"
           npmPackage "folio" "Folio" "@echelon-foundry/print-components" "0.3.0"
